b2162a13cb25566b5d73caf159a357f612d67095
IVP-LAPTOP87\rogsimer <rhejeen.ivp@gmail.com>
2018-11-20 17:23:49 +0800

    First commit

---
+++ Task/Task/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task.Models
+{
+    public class Project
+    {
+        public int id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
---
+++ Task/Task/Models/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Task.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public bool Complete { get; set; }
+        [Display(Name ="Due Date")]
+        public DateTime DueDate { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
